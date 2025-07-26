98a78a26eeb561f5f00f53138aa71a4c8fc13a62
Chaitanya Shinde <9chaitanyashinde@gmail.com>
2025-07-26 17:51:35 +0530

    Initial Update

--- components/connect.php
+++ components/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_name = 'mysql:host=sql204.infinityfree.com;dbname=if0_36400025_students_corner';
-$user_name = 'if0_36400025';
-$user_password = 'Chait9505';
+$db_name = 'your_database_name';
+$user_name = 'your_username';
+$user_password = 'your_password';
 
 $conn = new PDO($db_name, $user_name, $user_password);
-$connn=mysqli_connect('sql204.infinityfree.com','if0_36400025','Chait9505','if0_36400025_students_corner');
+$connn=mysqli_connect('your_host_name','your_username','your_password','your_database_name');
 ?>
